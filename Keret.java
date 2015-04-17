61477944802ef6f132fb5bd6db32ba3a30a6061d
unknown <jakybalazs@gmail.com>
2015-04-17 22:56:03 +0200

    proba3

--- Keret.java
+++ Keret.java
@@ -1,8 +1,15 @@
 package Prototype;
 
+
 public class Keret {
 	public static void main(String [] args)
 	{
-		
+		Command cmd = new Command();
+		try{
+			while(cmd.isEnded != true)
+				cmd.fun(cmd.readCommand());
+		}catch(Exception e){System.out.println(e.getMessage());}
 	}
+	
+	
 }
