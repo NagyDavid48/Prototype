de5a7c60425c1b05713d8303d1ef4e6ee46d928d
jakybalazs <jakybalazs@gmail.com>
2015-04-18 21:39:26 +0200

    Keret kis javítás

--- Keret.java
+++ Keret.java
@@ -16,7 +16,6 @@ public class Keret {
 		String infile = null;
 		String outfile = "output.txt";
 		BufferedReader reader;
-		String[] line;
 		PrintWriter writer;
 		if (args.length == 1)
 			infile = args[0];
@@ -27,8 +26,8 @@ public class Keret {
 		}
 		try{
 			writer=new PrintWriter(new FileWriter(outfile));
-			while(cmd.isEnded != true && (line = cmd.readCommand(reader))!=null)                   // amíg meg nem állt a program
-				cmd.fun(line, writer);              // olvassuk a parancsokat
+			while(cmd.isEnded != true)                   				// amíg meg nem állt a program
+				cmd.fun(cmd.readCommand(reader), writer);               // olvassuk a parancsokat
 			reader.close();
 			writer.close();
 		}catch(Exception e){/*System.out.println(e.getMessage())*/e.printStackTrace();}
